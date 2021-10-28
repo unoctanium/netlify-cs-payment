870a9c4b651a73425227d272b32c4688d7cba50e
Odo Maletzki <odo@maletzki.net>
2021-10-28 19:36:46 +0200

    stripe checkout first running test

--- src/lambda/stripe-checkout.js
+++ src/lambda/stripe-checkout.js
@@ -1,23 +1,82 @@
 
+import { parse } from 'querystring'
 
-const { ENV } = process.env;
+const { STRIPE_SECRET } = process.env;
+//PP_CLIENT_ID
+//PP_CLIENT_SECRET
 
-console.log(ENV);
 // Configure stripe
-//const stripe = require('stripe')(stripeConfig.secret);
+const stripe = require('stripe')(STRIPE_SECRET);
 
 export async function handler(event, context) {
+
+    // bail if wrong http Method
+    if (event.httpMethod != "POST") {
+        return {
+            statusCode: 405,
+            body: 'You are not using a http POST method for this endpoint.',
+            headers: {'Allow': 'POST'}
+          };        
+    }
+
+    // parse request body to json
+    let body = {}
+    try {
+      body = JSON.parse(event.body)
+    } catch (e) {
+      body = parse(event.body)
+    }
+
+    // Bail if parameters are missing
+    if (
+        !body["stripe-single-price-id"] || !body["stripe-subscription-price-id"] 
+        || !body["paypal-price"] || !body["paypal-product"] || !body["paypal-subscription-id"]
+        || !body.quantity || !body.mode || !body.payment
+        || !body["success-url"] || !body["cancel-url"]  
+        || !body.firstname || !body.lastname || !body.email 
+    ) {
+        return {
+            statusCode: 400,
+            body: 'Wrong usage of parameters.'
+        } 
+    }
+    
+    // set some parameters depending on purcase mode
+    const purchaseMode = (body.mode == "Monthly Subscription") ? "subscription" : "payment";
+    const purchasePrice = (purchaseMode == "payment") ? body["stripe-single-price-id"] : body["stripe-subscription-price-id"];
+
+    // create stripe checkout session
+    const session = await stripe.checkout.sessions.create({
+        line_items: [
+        {
+            price: purchasePrice,
+            quantity: body.quantity,
+        },
+        ],
+        payment_method_types: [
+        'card',
+        ],
+        mode: purchaseMode,
+        success_url: body['success-url'],
+        cancel_url: body['cancel-url'],
+        customer_email: body.email,
+        metadata: {
+            'firstname': body.firstname, 
+            'lastname': body.lastname,
+            'email': body.email
+        },
+        //submit_type: 'donate'
+    });
+
+    // redirect to success-url or cancel-url
     return {
-      statusCode: 200,
-      body: JSON.stringify({ message: `Hello world ${Math.floor(Math.random() * 10)}` })
+      statusCode: 303,
+      //body: {},
+      headers: {
+          location: session.url
+      }
     };
 }
-
-
-
-
-
-
 
 
 /*
